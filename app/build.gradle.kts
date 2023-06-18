938505a22ca30cb7d3752d4874ca442ac8fe93dd
denizk0461 <denizk0461@gmail.com>
2023-06-18 13:22:42 +0200

    Converted build.gradle files to Kotlinscript

---
+++ app/build.gradle.kts
@@ -0,0 +1,67 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+}
+
+android {
+    namespace = "com.denizk0461.bsag"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.denizk0461.bsag"
+        minSdk = 24
+        targetSdk = 33
+        versionCode = 1
+        versionName = "0.0.1"
+
+        resourceConfigurations += arrayOf("en", "de")
+
+        buildConfigField("long", "BUILD_TIME_MILLIS", "${System.currentTimeMillis()}L")
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        javaCompileOptions.annotationProcessorOptions {
+            arguments += mapOf("room.schemaLocation" to "${projectDir}/schemas")
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            signingConfig = signingConfigs.getByName("debug") // shouldn't this be "release"?
+
+            ndk {
+                debugSymbolLevel = "SYMBOL_TABLE"
+            }
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.fragment:fragment-ktx:1.7.0-alpha01")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.6.0")
+    implementation("androidx.navigation:navigation-ui-ktx:2.6.0")
+    implementation("androidx.preference:preference-ktx:1.2.0")
+    implementation("androidx.room:room-runtime:2.5.1")
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.2.0-alpha01")
+
+    kapt("androidx.room:room-compiler:2.5.1")
+
+    implementation("com.google.android.material:material:1.9.0")
+}
