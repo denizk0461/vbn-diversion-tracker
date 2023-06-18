dc5e0dd8ee7f568b2f9131d6f1092e9fcabf1d0e
denizk0461 <denizk0461@gmail.com>
2023-06-18 13:25:24 +0200

    Created package structure for Kotlin files

--- app/src/main/java/com/denizk0461/bsag/activity/MainActivity.kt
+++ app/src/main/java/com/denizk0461/bsag/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.denizk0461.bsag
+package com.denizk0461.bsag.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.denizk0461.bsag.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
